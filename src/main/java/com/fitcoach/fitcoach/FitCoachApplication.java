e42917cfa29474077af2f28e703ae8f12430b6c3
Souhail El Mokadem <devsouhail1@gmail.com>
2024-05-02 10:57:30 +0100

    initialisation

---
+++ src/main/java/com/fitcoach/fitcoach/FitCoachApplication.java
@@ -0,0 +1,13 @@
+package com.fitcoach.fitcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitCoachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitcoach/fitcoach/FitCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitcoach.fitcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
